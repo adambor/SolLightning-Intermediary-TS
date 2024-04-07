e24025fb653e38dc86d94f98a4b132fea818b2c1
adambor <adamborteam@gmail.com>
2024-04-07 11:10:10 +0200

    Update JITO, update libs

--- src/Jito.ts
+++ src/Jito.ts
@@ -5,6 +5,8 @@ const jitoPubkey = new PublicKey(
     "DttWaMuVvTiduZRnguLF7jNxTgiMBZ1hyAumKUiL2KRL"
 );
 
+const jitoEndpoint = "https://frankfurt.mainnet.block-engine.jito.wtf/api/v1/transactions";
+
 export class Jito {
 
     static applyJito(swapProgram: SolanaSwapProgram) {
@@ -12,14 +14,16 @@ export class Jito {
         swapProgram.onBeforeTxSigned((tx) => {
             if(tx.tx.signatures.length===0) {
                 const unitLimitIxIndex = tx.tx.instructions.findIndex(ix => ix.programId.equals(ComputeBudgetProgram.programId) && ix.data[0]===0x02);
-                const unitLimitIx: TransactionInstruction = unitLimitIxIndex==null ? null : tx.tx.instructions[unitLimitIxIndex];
+                const unitLimitIx: TransactionInstruction = unitLimitIxIndex==-1 ? null : tx.tx.instructions[unitLimitIxIndex];
+                // console.log("Unit limit IX: ", unitLimitIx?.data);
                 const unitLimit: number = unitLimitIx==null ? 200000 : unitLimitIx.data.readUint32LE(1);
-                if(unitLimitIxIndex!=null) tx.tx.instructions.splice(unitLimitIxIndex, 1);
+                if(unitLimitIxIndex!=-1) tx.tx.instructions.splice(unitLimitIxIndex, 1);
 
                 const unitPriceIxIndex = tx.tx.instructions.findIndex(ix => ix.programId.equals(ComputeBudgetProgram.programId) && ix.data[0]===0x03);
-                const unitPriceIx: TransactionInstruction = unitPriceIxIndex==null ? null : tx.tx.instructions[unitPriceIxIndex];
+                const unitPriceIx: TransactionInstruction = unitPriceIxIndex==-1 ? null : tx.tx.instructions[unitPriceIxIndex];
+                // console.log("Unit price IX: ", unitPriceIx?.data);
                 const unitPrice: bigint = unitPriceIx==null ? BigInt(8000) : unitPriceIx.data.readBigUint64LE(1);
-                if(unitPriceIxIndex!=null) tx.tx.instructions.splice(unitPriceIxIndex, 1);
+                if(unitPriceIxIndex!=-1) tx.tx.instructions.splice(unitPriceIxIndex, 1);
 
                 //Apply Jito to it
                 tx.tx.add(SystemProgram.transfer({
@@ -48,10 +52,12 @@ export class Jito {
                 return null;
             }
 
+            console.log("Send Jito tx, fee: ", decodedIxData.lamports);
+
             if(options==null) options = {};
 
             //Is Jito tx
-            const request = await fetch("https://mainnet.block-engine.jito.wtf/api/v1/transactions", {
+            const request = await fetch(jitoEndpoint, {
                 method: "POST",
                 body: JSON.stringify({
                     jsonrpc: "2.0",
@@ -69,7 +75,8 @@ export class Jito {
 
             if(request.ok) {
                 const parsedResponse = await request.json();
-                return parsedResponse.data.result;
+                console.log(parsedResponse);
+                return parsedResponse.result;
             }
 
             throw new Error(await request.text());
